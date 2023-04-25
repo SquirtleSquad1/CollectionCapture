52dcf9d03dc7a9e5d27517ccf79f1730ffc16943
Adam Liang <adam.liang@alumni.stonybrook.edu>
2023-04-25 00:11:37 -0400

    Added JSDocs for session.jsx

--- src/db/session.jsx
+++ src/db/session.jsx
@@ -1,6 +1,14 @@
 import { redirect } from "solid-start/server";
+// the native session storage by solid js
 import { createCookieSessionStorage } from "solid-start/session";
+// imports the database connection from src/db/index.jsx where a connection is established by prisma
 import { db } from ".";
+
+/**
+ * Register a user in the database
+ * @param {{ username: any; password: any; }} { username, password }
+ * @returns {unknown}
+ */
 export async function register({ username, password }) {
   return db.user.create({
     data: {
@@ -9,6 +17,12 @@ export async function register({ username, password }) {
     },
   });
 }
+
+/**
+ * Login: Uses prisma to find a user in the database
+ * @param {{ username: any; password: any; }} { username, password }
+ * @returns {unknown}
+ */
 export async function login({ username, password }) {
   const user = await db.user.findUnique({
     where: {
@@ -21,6 +35,8 @@ export async function login({ username, password }) {
   return user;
 }
 const sessionSecret = import.meta.env.SESSION_SECRET;
+
+
 const storage = createCookieSessionStorage({
   cookie: {
     name: "RJ_session",
@@ -34,15 +50,34 @@ const storage = createCookieSessionStorage({
     httpOnly: true,
   },
 });
+
+
+/**
+ * Get user session from cookie
+ * @param {*} request
+ * @returns {*}
+ */
 export function getUserSession(request) {
   return storage.getSession(request.headers.get("Cookie"));
 }
+
+/**
+ * Gets User ID from session
+ * @param {*} request
+ * @returns {unknown}
+ */
 export async function getUserId(request) {
   const session = await getUserSession(request);
   const userId = session.get("userId");
   if (!userId || typeof userId !== "string") return null;
   return userId;
 }
+/**
+ * Upon visiting a page, check if the user is logged in. If not, redirect to login page.
+ * @param {*} request
+ * @param {*} [redirectTo=new URL(request.url).pathname]
+ * @returns {unknown}
+ */
 export async function requireUserId(
   request,
   redirectTo = new URL(request.url).pathname
@@ -56,12 +91,19 @@ export async function requireUserId(
   return userId;
 }
 
+/**
+ * getUser is a function that takes a database and a request and returns a user
+ * @param {*} db
+ * @param {*} request
+ * @returns {unknown}
+ */
 export async function getUser(db, request) {
   const userId = await getUserId(request);
   if (typeof userId !== "string") {
     return null;
   }
   try {
+    // findUnique is a function that takes a query and returns a user
     const user = await db.user.findUnique({
       where: {
         id: userId,
@@ -72,6 +114,12 @@ export async function getUser(db, request) {
     throw logout(request);
   }
 }
+
+/**
+ * Logs out a user by destroying the session
+ * @param {*} request
+ * @returns {unknown}
+ */
 export async function logout(request) {
   const session = await storage.getSession(request.headers.get("Cookie"));
   return redirect("/login", {
@@ -80,6 +128,13 @@ export async function logout(request) {
     },
   });
 }
+
+/**
+ * Creates a session after user logs in
+ * @param {*} userId
+ * @param {*} redirectTo
+ * @returns {unknown}
+ */
 export async function createUserSession(userId, redirectTo) {
   const session = await storage.getSession();
   session.set("userId", userId);
--- src/db/useUser.jsx
+++ src/db/useUser.jsx
@@ -1,6 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import { createServerData$, redirect } from "solid-start/server";
 import { getUser } from "./session";
+
+
+/**
+ * Uses createServerData to connect to Prisma then uses getUser to get the user
+ * @returns {*}
+ */
 export const useUser = () =>
   createServerData$(async (_, { request }) => {
     const db = new PrismaClient();
