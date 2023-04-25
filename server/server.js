66808a9480fbe78f70a97ea159c57a420a493762
mjax <matt.d.jax@gmail.com>
2023-04-25 14:42:48 -0400

    able to add user and card to database

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const express = require("express")
 const mtg = require('mtgsdk')
 const app = express()
 const port = 3000;
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient()
+// use `prisma` in your application to read and write data in your DB
 
 // parse json request body
 app.use(express.json());
@@ -10,11 +14,33 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/api/cards', (req, res) => {
-    mtg.card.where(req.body)
-      .then(results => {
-        res.status(200).send(results)
-      })
+  mtg.card.where(req.body)
+    .then(results => {
+      res.status(200).send(results)
+    })
+})
+
+app.post('/api/cards', async (req, res) => {
+  console.log(req.body)
+  const { name, imageUrl } = req.body;
+  const newCard = await prisma.card.create({
+    data: {
+      name,
+      imageUrl,
+    }
+  })
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        username: 'test',
+        password: 'test',
+      }
+    })
+  } catch (e) {
+    console.log(e)
+  }
+  res.status(201).json(newCard);
 })
 app.listen(port, () => {
-    console.log(`Example app listening at PORT ${port}`)
+  console.log(`Example app listening at PORT ${port}`)
 })
