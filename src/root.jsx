794f0a0c5e9655811e5848ba4daa639b27ab3189
Adam Liang <adam.liang@alumni.stonybrook.edu>
2023-04-25 19:12:05 -0400

    Co-authored-by: Steven Wu <stevenox7@users.noreply.github.com>

--- src/root.jsx
+++ src/root.jsx
@@ -11,18 +11,20 @@ import {
   Scripts,
   Title,
 } from "solid-start";
+import Navbar from "./components/Navbar";
 import "./root.css";
 export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - With Auth</Title>
+        <Title>MTG - Gathering</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Body>
         <ErrorBoundary>
           <Suspense fallback={<div>Loading</div>}>
+            <Navbar />
             <Routes>
               <FileRoutes />
             </Routes>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,69 +1,10 @@
-import { refetchRouteData, useRouteData } from "solid-start";
-import { createServerAction$ } from "solid-start/server";
-import { logout } from "~/db/session";
-import { useUser } from "../db/useUser";
-export function routeData() {
-  return useUser();
-}
-export default function Home() {
-  const user = useRouteData();
-  const [, { Form }] = createServerAction$((f, { request }) => logout(request));
-  
+const Home = () => {
+
   return (
-    <main class="full-width">
-      <h1>Hello {user()?.username}</h1>
-      <h3>This sucks</h3>
-      <button onClick={() => refetchRouteData()}>Refresh</button>
-      <Form>
-        <button name="logout" type="submit">
-          Logout
-        </button>
-      </Form>
+    <main>
+      <h1>Home</h1>
     </main>
-  );
+  )
 }
 
-// import { createSignal, For } from "solid-js";
-// export default function Home() {
-//   const [cardName, setCardName] = createSignal("");
-//   const [cardData, setCardData] = createSignal(null);
-
-//   const handleSubmit = async (event) => {
-//     event.preventDefault();
-//     // handle form submission here, e.g. by pinging the express endpoint
-//     await fetch(`/api/getCards?name=${cardName()}`)
-//       .then((response) => response.json())
-//       .then((data) => setCardData(data));
-//   };
-
-//   const handleInputChange = (event) => {
-//     setCardName(event.target.value);
-//   };
-
-//   return (
-//     <main class="full-width">
-//       <h1 class="text-3xl font-bold underline">{cardName()}</h1>
-//       <form onSubmit={handleSubmit} id="card-search">
-//         <input
-//           type="text"
-//           placeholder="Name of card"
-//           value={cardName()}
-//           onInput={handleInputChange}
-//         />
-//         <button type="submit">Search</button>
-//       </form>
-
-//       <div class="border-2 border-black" id="card-display">
-//         {cardData() ? (
-//           <ul>
-//             <For each={cardData()}>{(card) => (
-//               <li >{card.name}</li>
-//             )}</For>
-//           </ul>
-//         ) : (
-//           <h3>No cards found</h3>
-//         )}
-//       </div>
-//     </main>
-//   );
-// }
+export default Home;
